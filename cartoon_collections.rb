14db698a4a27d5a4c998f46056312ad922a9d90e
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-03 17:46:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-    planeteer_calls.find {|element| elements.include? valid_calls }
+    planeteer_calls.find {|element| element.include? valid_calls }
   
   
 
